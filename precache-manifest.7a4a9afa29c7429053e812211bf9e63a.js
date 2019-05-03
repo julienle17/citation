d8c0f08636a9ed311a841c233c6045ace782666b
julienle17 <julienleclercq17@gmail.com>
2019-05-03 14:23:18 +0200

    Updates

---
+++ precache-manifest.7a4a9afa29c7429053e812211bf9e63a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a891efdc3fa656425b6e528f2d46f53a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24cf56557f52f908c43a",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "24cf56557f52f908c43a",
+    "url": "/static/js/main.0cc766c8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
