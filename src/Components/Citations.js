43fd60aeb5d0ef0caf872468cd7533d194717e5d
julienle17 <julienleclercq17@gmail.com>
2019-05-03 10:24:17 +0200

    add bulma  css in html & add state in component citation.js

---
+++ src/Components/Citations.js
@@ -0,0 +1,28 @@
+import React, { Component } form 'react';
+
+class Citations extends Component {
+	state = {
+		citations: [
+			{
+				author: `Gandhi`
+				citation: `La vie est un mystère qu'il faut vivre, et non un problème à résoudre.`
+			},
+			{
+				author: `Léonard De Vinci`
+				citation: `Tout obstacle renforce la détermination. Celui qui s'est fixé un but n'en change pas.`
+			},
+			{
+				author: `Albert Einstein`
+				citation: `La connaissance s'acquiert par l'expérience, tout le reste n'est que de l'information.`
+			}
+		]
+	};
+	render(){
+		return(
+
+		)
+	}
+
+}
+
+export default Citations
